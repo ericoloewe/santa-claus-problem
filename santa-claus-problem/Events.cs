7b85a6daef9d36a8ae199b168f34caa5f018d5f2
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-04-03 22:25:36 -0300

    style(events): sort lines

--- santa-claus-problem/Events.cs
+++ santa-claus-problem/Events.cs
@@ -5,19 +5,19 @@ namespace santa_claus_problem
 {
     public class NorthPoleEvents
     {
-        public Action OnSantaClausSleep { get; set; } = () => { };
         public Action OnSantaClausAwake { get; set; } = () => { };
+        public Action OnSantaClausSleep { get; set; } = () => { };
         public Action OnSantaDiscussToyProjects { get; set; } = () => { };
         public Action OnSantaGiveToys { get; set; } = () => { };
         public Action OnSantaTieReindeerGroup { get; set; } = () => { };
         public Action OnSantaUntieReindeerGroup { get; set; } = () => { };
+        public Action<ElveAwakeMessage> OnElvesAwakeSanta { get; set; } = i => { };
         public Action<int> OnCreateElve { get; set; } = i => { };
-        public Action<int> OnElveMeetSanta { get; set; } = i => { };
-        public Action<int> OnElveBuildToys { get; set; } = i => { };
-        public Action<int> OnReindeerGoToVacation { get; set; } = i => { };
         public Action<int> OnCreateReindeer { get; set; } = i => { };
+        public Action<int> OnElveBuildToys { get; set; } = i => { };
+        public Action<int> OnElveMeetSanta { get; set; } = i => { };
+        public Action<int> OnReindeerGoToVacation { get; set; } = i => { };
         public Action<int> OnReindeerMeetSanta { get; set; } = i => { };
         public Action<ReindeerAwakeMessage> OnReindeersAwakeSanta { get; set; } = i => { };
-        public Action<ElveAwakeMessage> OnElvesAwakeSanta { get; set; } = i => { };
     }
 }
